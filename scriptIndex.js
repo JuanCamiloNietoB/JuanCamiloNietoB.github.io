997095f1efc16031b9f147b39c273fd6d0a4c2c2
JuanCamiloNietoB <100132926+JuanCamiloNietoB@users.noreply.github.com>
2024-09-30 21:32:23 -0500

    limpiar

--- scriptIndex.js
+++
@@ -1,9 +0,0 @@
-// Mostrar/ocultar los botones según el estado del checkbox
-document.getElementById('toggleButtons').addEventListener('change', function() {
-    const buttons = document.getElementById('buttons');
-    if (this.checked) {
-        buttons.style.display = 'block'; // Mostrar botones
-    } else {
-        buttons.style.display = 'none'; // Ocultar botones
-    }
-});
